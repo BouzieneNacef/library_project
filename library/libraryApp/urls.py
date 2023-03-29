2a348bd587f21c76273cd52f80ad8dcd471b6c1f
BouzieneNacef <bouzaienne1995@gmail.com>
2023-03-29 03:54:21 +0200

    commit

--- library/libraryApp/urls.py
+++ library/libraryApp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from. import views
 
 urlpatterns=[
-    path("",views.home),
-    path("book/",views.book),
-    path("about/",views.about)
+    path("", views.ahla, name='ahla'),
+    path("home/", views.home, name='home'),
+    
+   
 ]
--- library/libraryApp/views.py
+++ library/libraryApp/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+def ahla(request):
+    return render(request, 'welcome.html')
 
 def home(request):
-    return HttpResponse("home")
+    return HttpResponse("this is the second page")
 
-def book(request):
-    return HttpResponse("book")
-
-def about(request):
-    return HttpResponse("about")
